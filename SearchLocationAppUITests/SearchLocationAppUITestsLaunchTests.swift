8f18a125de6dbc41916ea9e23d8b19ac2f5507ce
GitHub Kullanıcı Adınız <149234315+SarperKececi@users.noreply.github.com>
2024-02-18 13:51:09 +0300

    Initial Commit

---
+++ SearchLocationAppUITests/SearchLocationAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SearchLocationAppUITestsLaunchTests.swift
+//  SearchLocationAppUITests
+//
+//  Created by Sarper Kececi on 18.02.2024.
+//
+
+import XCTest
+
+final class SearchLocationAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
